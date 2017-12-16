040ab48b539a7b94420737d288f72fc6c27085e0
David Lavery <dlavery62@gmail.com>
2017-12-16 16:27:59 +0000

    improve logging

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_pymongo import PyMongo
-from logging import FileHandler
+from logging.handlers import TimedRotatingFileHandler
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'cam'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/cam'
 
-fh = FileHandler('cam-api.log', mode='a', encoding='utf8', delay=False)
+fh = TimedRotatingFileHandler('cam-api.log', when='midnight', encoding='utf8', delay=False, utc=True)
 app.logger.addHandler(fh)
 
 mongo = PyMongo(app)
